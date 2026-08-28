90c7fd7d82c39bf9d1fb6fb9c29159e44d043d37
Nguyen Ly <thily08102005@gmail.com>
2026-08-28 19:46:29 +0700

    23737701- Configure Android platform

---
+++ android/app/.cxx/Debug/6p233m72/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::hermesvm)
+add_library(hermes-engine::hermesvm SHARED IMPORTED)
+set_target_properties(hermes-engine::hermesvm PROPERTIES
+    IMPORTED_LOCATION "C:/Users/OS/.gradle/caches/9.4.1/transforms/cb74f671c90fd9062c26e633f1d2a6cd/transformed/hermes-android-250829098.0.17-debug/prefab/modules/hermesvm/libs/android.armeabi-v7a/libhermesvm.so"
+    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/OS/.gradle/caches/9.4.1/transforms/cb74f671c90fd9062c26e633f1d2a6cd/transformed/hermes-android-250829098.0.17-debug/prefab/modules/hermesvm/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
